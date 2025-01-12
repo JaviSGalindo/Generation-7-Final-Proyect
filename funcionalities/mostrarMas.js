751392fa557ce29c3eb162545444e00d5617fc22
Andres Gomez <andresxv98@gmail.com>
2025-01-12 18:03:40 -0500

    ajustes fn btn mostrar más

--- funcionalities/mostrarMas.js
+++ funcionalities/mostrarMas.js
@@ -1,12 +1,11 @@
 document.getElementById("toggleButton").addEventListener("click", function () {
-    const infoDiv = document.getElementById("info");
-    
+  const infoDiv = document.getElementById("info");
 
-    if (infoDiv.style.display === "none") {
-        infoDiv.style.display = "block";
-        this.textContent = "...";
-    } else {
-        infoDiv.style.display = "none";
-        this.textContent = "Descubre más sobre nosotros";
-    }   
+  if (infoDiv.classList.contains("visible")) {
+    infoDiv.classList.remove("visible");
+    this.textContent = "Mostrar menos";
+  } else {
+    infoDiv.classList.add("visible");
+    this.textContent = "Descubre más";
+  }
 });
