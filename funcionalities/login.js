430dba2a20ccd6be2bf0db3b02c0ebd4d1a86d73
Johana Rincon <johanamurillorincon@gmail.com>
2025-01-29 22:08:22 -0500

    login y recuperacion

---
+++ funcionalities/login.js
@@ -0,0 +1,9 @@
+// Alternar visibilidad de la contraseña
+const togglePassword = document.getElementById("togglePassword");
+const passwordField = document.getElementById("password");
+
+togglePassword.addEventListener("click", function () {
+  const type = passwordField.type === "password" ? "text" : "password";
+  passwordField.type = type;
+  this.classList.toggle("fa-eye-slash");
+});
